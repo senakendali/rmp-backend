f322e94e6fd7221a1b11aa86b0dc81ea424f0540
Sena Kendali <sena.kendali@gmail.com>
2024-12-07 08:51:18 +0700

    update table structure in goos_category table

---
+++ database/migrations/2024_12_07_012132_add_foreign_key_to_vendor_histories.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('vendor_histories', function (Blueprint $table) {
+            $table->foreign('vendor_id')->references('id')->on('vendors')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('vendor_histories', function (Blueprint $table) {
+            $table->dropForeign(['vendor_id']); // Drop the foreign key
+        });
+    }
+};
